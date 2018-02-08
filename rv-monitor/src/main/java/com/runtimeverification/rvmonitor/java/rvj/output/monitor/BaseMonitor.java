6a39a6a53db46469d31b383549e6a0cfb58c9363
Owolabi Legunsen <legunse2@illinois.edu>
2018-02-08 01:12:53 -0600

    automating changes to spec generation so that they only print dynamic violations once

--- rv-monitor/src/main/java/com/runtimeverification/rvmonitor/java/rvj/output/monitor/BaseMonitor.java
+++ rv-monitor/src/main/java/com/runtimeverification/rvmonitor/java/rvj/output/monitor/BaseMonitor.java
@@ -623,13 +623,17 @@ public class BaseMonitor extends Monitor {
             }
 
             // Generate code to trigger handler
-            ret += "if(" + monitorVar + "." + rvmVariable + ") {\n";
+            ret += "if(" + monitorVar + "." + rvmVariable + " && " + monitorVar + ".violationCount == 0 ) {\n";
 
             ret += monitorVar + "." + handlerMethod.getMethodName() + "(";
-            if (!Main.stripUnusedParameterInMonitor)
+            if (!Main.stripUnusedParameterInMonitor) {
                 ret += event.getRVMParametersOnSpec().parameterStringIn(
                         specParam);
+            }
             ret += ");\n";
+            ret += "// added for reducing printing\n";
+            ret += "System.out.println(" + monitorVar + ".violationCount); \n";
+            ret += monitorVar + ".violationCount++;\n";
 
             ret += "}\n";
         }
@@ -825,6 +829,8 @@ public class BaseMonitor extends Monitor {
             }
         }
         ret += "\n";
+
+        ret += "public int violationCount = 0;\n";
 
         if (this.isAtomicMoniorUsed()) {
             ICodeFormatter fmt = CodeFormatters.getDefault();
